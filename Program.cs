00a03df019231603ecbf1bbabf15729f96de94b6
SERGEY <96051914+Sergeiphantom86@users.noreply.github.com>
2025-03-28 22:20:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 class Weapon
 {
-    private const int MinBulletsToShoot = 0;
     private const int BulletsPerShot = 1;
-
     private int _bullets;
 
-    public bool CanShoot() => _bullets > MinBulletsToShoot;
+    public bool CanShoot => _bullets >= BulletsPerShot;
 
-    public void Shoot() => _bullets -= BulletsPerShot;
+    public void Shoot()
+    {
+        if (CanShoot == false)
+        {
+            throw new InvalidOperationException("Недостаточно пуль для выстрела");
+        }
+
+        _bullets -= BulletsPerShot;
+    }
 }
